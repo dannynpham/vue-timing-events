2345fd764503f2c97638d77a4a7dc245b02e7377
Danny Pham <dannynpham@csu.fullerton.edu>
2021-01-22 11:06:24 -0800

    Add timing event utility.

---
+++ index.js
@@ -0,0 +1,61 @@
+// Install the components
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
+export default (Vue) => {
+  const INTERVALS = 'intervals';
+  const TIMEOUTS = 'timeouts';
+  const LISTENERS = 'listeners';
+  Vue.prototype.$componentListeners = new Map();
+
+  Vue.mixin({
+    created() {
+      const listeners = new Map([
+        [INTERVALS, []],
+        [TIMEOUTS, []],
+        [LISTENERS, []],
+      ]);
+      this.$componentListeners.set(this._uid, listeners);
+    },
+    beforeDestroy() {
+      this._cleanup(INTERVALS, window.clearInterval);
+      this._cleanup(TIMEOUTS, window.clearTimeout);
+      this._cleanupListener();
+
+      this.$componentListeners.delete(this._uid);
+    },
+    methods: {
+      _cleanup(type, func) {
+        const compListener = this.$componentListeners.get(this._uid);
+        const events = compListener.get(type);
+        events.forEach((event) => func(event));
+      },
+      _cleanupListener() {
+        const compListener = this.$componentListeners.get(this._uid);
+        const listeners = compListener.get(LISTENERS);
+        listeners.forEach(([event, callback]) => window.removeEventListener(event, callback));
+      },
+    },
+  });
+
+  Vue.prototype.$setInterval = function $setInterval(callback, interval) {
+    const compListener = this.$componentListeners.get(this._uid);
+    const intervals = compListener.get(INTERVALS);
+
+    intervals.push(window.setInterval(callback, interval));
+  };
+
+  Vue.prototype.$setTimeout = function $setTimeout(callback, timeout) {
+    const compListener = this.$componentListeners.get(this._uid);
+    const timeouts = compListener.get(TIMEOUTS);
+
+    timeouts.push(window.setTimeout(callback, timeout));
+  };
+
+  Vue.prototype.$addEventListener = function $addEventListener(event, callback, opts) {
+    const compListener = this.$componentListeners.get(this._uid);
+    const listeners = compListener.get(LISTENERS);
+
+    listeners.push([event, callback]);
+    window.addEventListener(event, callback, opts);
+  };
+};
